8ffd9ef7a2a244530cc65e45a6291cc25629bc52
kishlayswapnil <kishlayswapnil@gmail.com>
2020-04-18 05:28:06 +0530

    Added: The User service interface has been added.

---
+++ Fundoo Notes/src/main/java/com/bridgelabz/fundoonotes/service/UserService.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.fundoonotes.service;
+
+import com.bridgelabz.fundoonotes.dto.LoginDto;
+import com.bridgelabz.fundoonotes.dto.RegisterDto;
+import com.bridgelabz.fundoonotes.model.Response;
+
+public interface UserService {
+    Response login(LoginDto user);
+
+    Response register(RegisterDto userRecord);
+}
