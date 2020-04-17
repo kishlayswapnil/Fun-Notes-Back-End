1113e735686768da8a9144061d50e70a1781ce15
kishlayswapnil <kishlayswapnil@gmail.com>
2020-04-18 04:08:14 +0530

    Refactored: Response Entity added in the REST Api.

--- Fundoo Notes/src/main/java/com/bridgelabz/fundoonotes/controller/UserController.java
+++ Fundoo Notes/src/main/java/com/bridgelabz/fundoonotes/controller/UserController.java
@@ -1,12 +1,16 @@
 package com.bridgelabz.fundoonotes.controller;
 
 import com.bridgelabz.fundoonotes.dto.LoginDto;
-import com.bridgelabz.fundoonotes.dto.UserDto;
+import com.bridgelabz.fundoonotes.dto.RegisterDto;
+import com.bridgelabz.fundoonotes.model.Response;
 import com.bridgelabz.fundoonotes.repository.UserRepository;
+import com.bridgelabz.fundoonotes.serviceimplimentation.UserServiceImplements;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import javax.validation.Valid;
 
 //For creating a REST api.
 @RestController
@@ -14,18 +18,22 @@ public class UserController {
 
     //Injecting the object dependency.
     @Autowired
-    UserRepository repos;
+    UserRepository userRepository;
+
+    @Autowired
+    UserServiceImplements userService;
 
     //Login API with mapping.
-    @RequestMapping("/login")
-    public LoginDto login(LoginDto loginDto) {
-        return loginDto;
+    @PostMapping("/login")
+    public ResponseEntity<Response> login(@Valid @RequestBody LoginDto loginDto) {
+        Response response = userService.login(loginDto);
+        return new ResponseEntity(response, HttpStatus.OK);
     }
 
     //Registration API with mapping.
-    @RequestMapping("/registration")
-    public List<UserDto> getRegister() {
-        List<UserDto> user = (List<UserDto>) repos.findAll();
-        return user;
+    @PostMapping("/registration")
+    public ResponseEntity<Response> register(@Valid @RequestBody RegisterDto registerDto) {
+        Response response = userService.register(registerDto);
+        return new ResponseEntity(response, HttpStatus.OK);
     }
 }
