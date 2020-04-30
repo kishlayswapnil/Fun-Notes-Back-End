fde1de6852a220206723838a80c715a083906eef
kishlayswapnil <kishlayswapnil@gmail.com>
2020-04-30 11:45:15 +0530

    Added: few extra variables has been added for data transfer.

--- Fundoo Notes/src/main/java/com/bridgelabz/fundoonotes/model/Label.java
+++ Fundoo Notes/src/main/java/com/bridgelabz/fundoonotes/model/Label.java
@@ -1,10 +1,14 @@
 package com.bridgelabz.fundoonotes.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 import org.springframework.data.annotation.Id;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 public class Label {
@@ -12,8 +16,19 @@ public class Label {
     @Id
     private int labelId;
     private String name;
-    private int userId;
+    private LocalDateTime createdDate;
+    private LocalDateTime modifiedDate;
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JsonIgnore
+    private User user;
+    @JsonIgnore
+    @ManyToMany(mappedBy = "label")
+    private Set<Note> notes;
 
+    public Label() {
+    }
     @javax.persistence.Id
     public int getLabelId() {
         return labelId;
@@ -31,11 +46,44 @@ public class Label {
         this.name = name;
     }
 
-    public int getUserId() {
-        return userId;
+    public User getUser() {
+        return user;
     }
 
-    public void setUserId(int userId) {
-        this.userId = userId;
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public LocalDateTime getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(LocalDateTime createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public LocalDateTime getModifiedDate() {
+        return modifiedDate;
+    }
+
+    public void setModifiedDate(LocalDateTime modifiedDate) {
+        this.modifiedDate = modifiedDate;
+    }
+
+    public boolean addNote(Note note) {
+        if(notes == null)
+            notes = new HashSet<>();
+        return notes.add(note);
+    }
+    public Set<Note> getNotes() {
+        return notes;
+    }
+
+    public void setNotes(Set<Note> notes) {
+        this.notes = notes;
+    }
+
+    public boolean removeNote(Note note) {
+        return notes.remove(note);
     }
 }
