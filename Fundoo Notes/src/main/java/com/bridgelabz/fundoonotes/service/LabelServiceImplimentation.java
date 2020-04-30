4fbf216cc1acef0b542bb131c6e31bb28b1421d9
kishlayswapnil <kishlayswapnil@gmail.com>
2020-04-30 13:05:48 +0530

    Added: Get Label methods has been implimented in service Implimentation.

--- Fundoo Notes/src/main/java/com/bridgelabz/fundoonotes/service/LabelServiceImplimentation.java
+++ Fundoo Notes/src/main/java/com/bridgelabz/fundoonotes/service/LabelServiceImplimentation.java
@@ -16,6 +16,7 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 import java.util.Optional;
+import java.util.Set;
 
 @Service
 @PropertySource("classpath:status.properties")
@@ -45,6 +46,7 @@ public class LabelServiceImplimentation implements LabelService{
             throw new LabelException(environment.getProperty("status.label.duplicate.error"), Integer.parseInt(environment.getProperty("status.label.errorCode")));
         Label label = modelMapper.map(labelDto, Label.class);
         label.setName(label.getName());
+        label.setUser(user);
         label = labelRepository.save(label);
         if(label == null)
             throw new LabelException(environment.getProperty("status.label.create.error"), Integer.parseInt(environment.getProperty("status.label.errorCode")));
@@ -59,13 +61,13 @@ public class LabelServiceImplimentation implements LabelService{
             return ResponseInfo.getResponse(Integer.parseInt(environment.getProperty("status.login.errorCode")),environment.getProperty("status.user.existErrorr"));
 
         //Check existence
-        if(!labelRepository.findByIdAndUser(userId, userData.get()).isPresent())
+        if(!labelRepository.findByIdAndUser(id, userData.get()).isPresent())
             return  ResponseInfo.getResponse(Integer.parseInt(environment.getProperty("status.label.errorCode")), environment.getProperty("status.label.exists.error"));
 
         //Duplicity
         if(labelRepository.findByNameAndUser(labelDto.getName(), userData.get()).isPresent())
             response = ResponseInfo.getResponse(Integer.parseInt(environment.getProperty("status.label.errorCode")), environment.getProperty("status.label.duplicate.error"));
-        Label label = labelRepository.findByIdAndUser(userId,userData.get()).get();
+        Label label = labelRepository.findByIdAndUser(id,userData.get()).get();
         modelMapper.map(labelDto, label);
         label.setName(label.getName());
         label = labelRepository.save(label);
@@ -96,5 +98,21 @@ public class LabelServiceImplimentation implements LabelService{
         else
             response = ResponseInfo.getResponse(Integer.parseInt(environment.getProperty("status.success.code")), environment.getProperty("status.label.delete.success"));
         return response;
+    }
+
+    @Override
+    public Set<Label> getAllLabels(String token) throws UserException{
+        int userId = tokenGenerator.retrieveIdFromToken(token);
+        Optional<User> userData = userRepository.findById(userId);
+        if(!(userData.isPresent() && userData.get().isVerified()))
+            throw new UserException(Integer.parseInt(environment.getProperty("status.login.errorCode")),environment.getProperty("status.user.existError"));
+        return labelRepository.findAllByUser(userData.get());
+    }
+
+    public Label getLabel(int id, String token) {
+        int userId = tokenGenerator.retrieveIdFromToken(token);
+        Optional<User> userData = userRepository.findById(userId);
+        Label label = labelRepository.findByIdAndUser(id, userData.get()).get();
+        return label;
     }
 }
